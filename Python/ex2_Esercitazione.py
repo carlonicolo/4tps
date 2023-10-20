e81a08d2b1ad72d0257cf75ca6e35184bc6d23e8
Carlo <carlo.nicolo@hotmail.com>
2023-10-20 23:31:21 +0200

    Esercitazione variabili

---
+++ Python/ex2_Esercitazione.py
@@ -0,0 +1,25 @@
+# Creare due variabili di tipo intero
+
+# Prima variabile
+...
+
+# Seconda variabile
+...
+
+# Fare la somma delle due variabile create precedentemente
+somma = ...
+
+# Print della variabile somma
+print("La somma di var1 + var2 è: " + str(somma))
+
+# se la somma è uguale a 10 print "la somma è uguale a 10"
+if(... == 10):
+    print("La somma è uguale a dieci. Variabile somma: " + ...)
+
+# se la somma è maggiore di 10 print "la somma è maggiore di 10"
+if(... > 10):
+    print("La somma è maggiore di dieci. Variabile somma: " + ...)
+
+# se la somma è minore di 10 "la somma è minore di 10"
+if(... < 10):
+    print("La somma è minore di dieci. Variabile somma: " + ...)
