e53ce16f103cb0841442797294fadcf8daba0b31
Carlo <carlo.nicolo@hotmail.com>
2024-02-18 00:29:44 +0100

    makefile

--- C++/OOP/PrimoEsempio/main.cpp
+++ C++/OOP/PrimoEsempio/main.cpp
@@ -3,19 +3,20 @@
 using namespace std;
 #include "Mathematics.hpp"
 
-int main() {
+int main()
+{
 	int num1, num2, result;
 	Mathematics maths;
-	cout <<"Enter the first number:";
-	cin>>num1;
-	cout<<"Enter the 2nd number:";
-	cin>>num2;
+	cout << "Enter the first number:";
+	cin >> num1;
+	cout << "Enter the 2nd number:";
+	cin >> num2;
 	result = maths.add(num1, num2);
-	cout <<"\nThe result of adding two numbers is: "<<result<<endl;
+	cout << "\nThe result of adding two numbers is: " << result << endl;
 	result = maths.subtract(num1, num2);
-	cout <<"The result of subtracting two numbers is: "<<result<<endl;
+	cout << "The result of subtracting two numbers is: " << result << endl;
 	result = maths.multiply(num1, num2);
-	cout <<"The result of multipltying two numbers is: "<<result<<endl;
+	cout << "The result of multipltying two numbers is: " << result << endl;
 	result = maths.divide(num1, num2);
-	cout <<"The result of dividing two numbers is: "<<result<<endl;
+	cout << "The result of dividing two numbers is: " << result << endl;
 }
