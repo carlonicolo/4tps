06101d0ad8ef19dc66e8b7cce966b5cb941f5b79
Carlo <carlo.nicolo@hotmail.com>
2024-10-25 22:21:07 +0200

    primo esercizio python

---
+++ 2024_25/Python/FirstSteps/1-start.py
@@ -0,0 +1,19 @@
+'''
+Primo programma Python
+Tipi variabili
+Per eseguire il programma: python [nome_programma]
+'''
+var = 5         # int
+var1 = 5.0      # float
+var2 = "5"      # str
+var3 = True     # bool
+var4 = ["Pippo","Pluto","Jack","Jimmy","Billy"]     # list
+
+print("var -> ", type(var))
+print("var1 -> ", type(var1))
+print("var2 -> ", type(var2))
+print("var3 -> ", type(var3))
+print("var3 -> ", type(var4))
+print("")
+print("Il valore dell'elemento var4[0] è: ", var4[0])
+print("Hello world")
