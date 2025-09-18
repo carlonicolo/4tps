b77f860cf600053dd6577c51b5f89bc4d408ec75
Carlo <carlo.nicolo@hotmail.com>
2025-09-18 20:05:54 +0200

    New exercise folder 2526

---
+++ 2526/Python/Modulo1/es1.py
@@ -0,0 +1,14 @@
+'''
+Questo è un commento su più linee
+'''
+# Questo è un commento su una singola linea
+
+'''
+This is a simple Python script that computes the sum 
+of two numbers and prints the result.
+'''
+# Define two numbers
+num1 = 10
+num2 = 50
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is:", sum)
---
+++ 2526/Python/Modulo1/es2.py
@@ -0,0 +1,16 @@
+'''
+This is a simple Python script that computes the 
+product of two numbers and prints the result. 
+'''
+
+# Declare and assign a value to num1
+num1 = 10
+
+# Declare and assign a value to num2
+num2 = 20
+
+# Compute num1*num2
+res = num1 * num2
+
+# Print the result of num1*num2
+print("The result of num1*num2 is: ", res)
